0bd79408a91ebeefec3966a5c85a54314757998a
Alibek Ergashev <ergashevalibek47@gmail.com>
2023-05-31 18:50:47 +0500

    JS lesson one task

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let firstname = " Alibek"
+let surname = " Ergashev"
+let age = 18
+let republic = "Uzbekistan"
+let city = "Karshi"
+let education = " TATU"
+let hobby = " Car drive"
+let work = " Sultan Wedding Studio"
+
+
+console.log("My name is" + firstname)
+console.log("My surname is" + surname )
+console.log("My age is " + age )
+console.log("My republic is " + republic)
+console.log("My city is " + city)
+console.log("My education is " + education)
+console.log("My hobby is" + hobby)
+console.log("My work is" + work )
+
+
+
+
+// let firstname = " Alibek ";
+// let num = 18  ;
+// let brand = "AliStore";
+// console.log(typeof brand);
+// console.log(num + firstname + firstname);
